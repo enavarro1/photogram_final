f079468f72fb0bfbda09b874e78d470626e8e5a4
Raghu Betina <raghu@firstdraft.com>
2020-02-24 21:54:11 +0000

    Direct association between users and follow_requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :received_follow_requests,
+             :class_name => "FollowRequest",
+             :foreign_key => "recipient_id",
+             :dependent => :destroy
+
   has_many   :photos,
              :foreign_key => "poster_id",
              :dependent => :destroy
